803e1ea065a138eaff79401e45c6a210647e1344
hvphuc <hvphuc081299@gmail.com>
2020-10-28 13:32:17 +0700

    remove root page

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,5 @@ Rails.application.routes.draw do
 		end	
 	end
 
-	root :to => "boards#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
